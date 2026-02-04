923ccee07c0616f0f143a6f4a392654688c1d810
Saht05 <nnlf0976@leeds.ac.uk>
2026-02-04 01:27:50 +0000

    fe

--- worksheet/task2/question5.sql
+++ worksheet/task2/question5.sql
@@ -3,4 +3,4 @@
 -- Expected Columns:
 -- StudentId, FirstName, LastName, TotalCreditsPassed
 
-Select Student.StudentId, FirstName, LastName, SUM(Credits) As TotalCreditsPassed  From Enrolment Join Student On Enrolment.StudentId=Student.StudentId Join Course On Enrolment.CourseId=Course.CourseId where grade>=40 group by Student.StudentId ;
+Select Student.StudentId, FirstName, LastName, SUM(Credits) As TotalCreditsPassed  From Enrolment Join Student On Enrolment.StudentId=Student.StudentId Join Course On Enrolment.CourseId=Course.CourseId where grade>=40 group by Student.StudentId, FirstName, LastName ;
